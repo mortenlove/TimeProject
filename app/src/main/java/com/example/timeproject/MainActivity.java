677bb37fd6b4d75433e00c4be19ebfd44efb0123
mortenlove <115878787+mortenlove@users.noreply.github.com>
2022-12-11 23:41:16 +0300

    1.2.0

--- app/src/main/java/com/example/timeproject/MainActivity.java
+++ app/src/main/java/com/example/timeproject/MainActivity.java
@@ -38,17 +38,50 @@ class Terms
         isaterms = currtime >= convtime & currtime <= convtime1;
         return isaterms;
     }
-    // ПЕЕМЕНА
+    // ПЕРЕМЕНА
     public boolean isaterms1;
 
     public boolean getTerms1(int convtime, int convtime1, int currtime)
     {
-        isaterms1 = currtime > convtime & currtime < convtime1;
+        isaterms1 = currtime <= convtime & currtime >= convtime1;
         return isaterms1;
     }
 }
 
 class LessonsCalc
+{
+
+
+    Convert conv = new Convert();
+    Terms terms = new Terms();
+
+    public String lefttime;
+
+    public String getLefttime(int hour, int minutes, int seconds, int[] startlessonhours,int[] endlessonhour, int[] startlessonminutes, int[] endlessonminutes, int[] numberlesson)
+    {
+        for (int i = 0; i < startlessonhours.length; i++)
+        {
+            if (terms.getTerms(conv.getConvert(startlessonhours[i], startlessonminutes[i]), conv.getConvert(endlessonhour[i], endlessonminutes[i]), conv.getConvert(hour, minutes)))
+            {
+                lefttime = "До конца " + numberlesson[i] + " урока: " + (conv.getConvert(endlessonhour[i], endlessonminutes[i]) - conv.getConvert(hour, minutes)) + " минут " + (60 - seconds) + " секунд.";
+            }
+            else
+            {
+                if (i != 0)
+                {
+                    if (terms.getTerms1(conv.getConvert(hour,minutes), conv.getConvert(startlessonhours[i], startlessonminutes[i]), conv.getConvert(endlessonhour[i-1], endlessonminutes[i-1])))
+                    {
+                        lefttime = "До конца перемены между " + numberlesson[i-1] + " и " + numberlesson[i]  + "уроками: " + (conv.getConvert(startlessonhours[i] ,startlessonminutes[i])-conv.getConvert(hour,minutes)) + " минут " + (60 - seconds) + " секунд.";
+                    }
+                }
+            }
+        }
+        return lefttime;
+    }
+
+}
+
+public class MainActivity extends AppCompatActivity
 {
 
     final int[] nomondaystartlessonshours = new int[] {8,8,9,10,11,12,13,13,14,15,16,16,17,18};
@@ -57,68 +90,16 @@ class LessonsCalc
     final int[] nomondaystartlessonsminutes = new int[] {0,50,35,20,15,0,0,45,30,15,5,50,35,20};
     final int[] nomondayendlessonsminutes = new int[] {40,30,15,0,55,40,40,25,10,55,45,30,15,0};
 
-    Convert conv = new Convert();
-    Terms terms = new Terms();
-    public String lefttime;
-    public String getNoMonday(int hour, int minutes, int seconds)
-    {
+    final int[] nomondaynumberlessons = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
 
-        for (int i = 0; i < nomondaystartlessonshours.length; i++)
-        {
-            if (terms.getTerms(conv.getConvert(nomondaystartlessonshours[i], nomondaystartlessonsminutes[i]), conv.getConvert(nomondayendlessonshours[i], nomondayendlessonsminutes[i]), conv.getConvert(hour, minutes)))
-            {
-//                timetxt.setText("До конца урока: " + (conv.getConvert(endlessonshours[i], endlessonsminutes[i]) - conv.getConvert(hour, minutes)) + " минут.");
-                lefttime = "До конца урока: " + (conv.getConvert(nomondayendlessonshours[i], nomondayendlessonsminutes[i]) - conv.getConvert(hour, minutes)) + " минут "  + (60 - seconds) + " секунд.";
-            }
-            else
-            {
-                if (i != 0)
-                {
-                    if (conv.getConvert(hour, minutes) <= conv.getConvert(nomondaystartlessonshours[i], nomondaystartlessonsminutes[i]) & conv.getConvert(hour, minutes) >= conv.getConvert(nomondayendlessonshours[i - 1], nomondayendlessonsminutes[i - 1]))
-                    {
-//                        timetxt.setText("До конца перемены: " + (conv.getConvert(startlessonshours[i] ,startlessonsminutes[i])-conv.getConvert(hour,minutes)) + " минут.");
-                        lefttime = "До конца перемены: " + (conv.getConvert(nomondaystartlessonshours[i] ,nomondaystartlessonsminutes[i])-conv.getConvert(hour,minutes)) + " минут " + (60 - seconds) + " секунд.";
-                    }
-                }
-            }
-        }
-        return lefttime;
-    }
 
-    public String getMonday(int hour, int minutes, int seconds)
-    {
-        final int[] mondaystartlessonshours = new int[] {7,8,9,9,10,11,12,13,13,14,15,15,16,17,18,18};
-        final int[] mondaystartlessonsminutes = new int[] {45,15,0,45,30,25,10,10,40,25,10,55,40,25,10,55};
+    final int[] mondaystartlessonshours = new int[] {7,8,9,9,10,11,12,13,13,14,15,15,16,17,18,18};
+    final int[] mondaystartlessonsminutes = new int[] {45,15,0,45,30,25,10,10,40,25,10,55,40,25,10,55};
 
-        final int[] mondayendlessonshours = new int[] {8,8,9,10,11,12,12,13,14,15,15,16,17,18,18,19};
-        final int[] mondayendlessonsminutes = new int[] {10,55,40,25,10,5,50,35,20,5,50,35,20,5,50,35};
+    final int[] mondayendlessonshours = new int[] {8,8,9,10,11,12,12,13,14,15,15,16,17,18,18,19};
+    final int[] mondayendlessonsminutes = new int[] {10,55,40,25,10,5,50,35,20,5,50,35,20,5,50,35};
 
-        for (int i = 0; i < mondaystartlessonshours.length; i++)
-        {
-            if (terms.getTerms(conv.getConvert(mondaystartlessonshours[i], mondaystartlessonsminutes[i]), conv.getConvert(mondayendlessonshours[i], mondayendlessonsminutes[i]), conv.getConvert(hour, minutes)))
-            {
-//                timetxt.setText("До конца урока: " + (conv.getConvert(endlessonshours[i], endlessonsminutes[i]) - conv.getConvert(hour, minutes)) + " минут.");
-                lefttime = "До конца урока: " + (conv.getConvert(mondayendlessonshours[i], mondayendlessonsminutes[i]) - conv.getConvert(hour, minutes)) + " минут " + (60 - seconds) + " секунд.";
-            }
-            else
-            {
-                if (i != 0)
-                {
-                    if (conv.getConvert(hour, minutes) <= conv.getConvert(mondaystartlessonshours[i], mondaystartlessonsminutes[i]) & conv.getConvert(hour, minutes) >= conv.getConvert(mondayendlessonshours[i - 1], mondayendlessonsminutes[i - 1]))
-                    {
-//                        timetxt.setText("До конца перемены: " + (conv.getConvert(startlessonshours[i] ,startlessonsminutes[i])-conv.getConvert(hour,minutes)) + " минут.");
-                        lefttime = "До конца перемены: " + (conv.getConvert(mondaystartlessonshours[i] ,mondaystartlessonsminutes[i])-conv.getConvert(hour,minutes)) + " минут " + (60 - seconds) + " секунд.";
-                    }
-                }
-            }
-        }
-        return lefttime;
-    }
-}
-
-public class MainActivity extends AppCompatActivity
-{
-
+    final int[] mondaynumberlessons = new int[] {0,1,2,3,4,5,6,0,7,8,9,10,11,12,13,14};
 
     int lesson;
     @Override
@@ -129,7 +110,6 @@ public class MainActivity extends AppCompatActivity
 
         final TextView timetxt = (TextView) findViewById(R.id.textView);
         final TextView outtime = (TextView) findViewById(R.id.textView2);
-        timetxt.setFontFeatureSettings("Times New Roman");
 
         Timer timer = new Timer();
         LessonsCalc lessons = new LessonsCalc();
@@ -145,19 +125,24 @@ public class MainActivity extends AppCompatActivity
                     public void run()
                     {
                         ZonedDateTime zone = ZonedDateTime.now();
-                        int hour = zone.getHour();
-                        int minutes = zone.getMinute();
+                        int hour = 13;//zone.getHour();
+                        int minutes = 15;//zone.getMinute();
                         int seconds = zone.getSecond();
-                        DayOfWeek dayofweek = zone.getDayOfWeek();
+                        //DayOfWeek dayofweek = zone.getDayOfWeek();
+                        String dayofweek = "TUESDAY";
                         System.out.println(dayofweek.toString());
                         outtime.setText(hour + ":" + minutes + ":" + seconds);
                         if (dayofweek.toString() == "MONDAY")
                         {
-                            timetxt.setText(lessons.getMonday(hour,minutes,seconds));
+                            timetxt.setText(lessons.getLefttime(hour,minutes,seconds,mondaystartlessonshours,mondayendlessonshours,mondaystartlessonsminutes,mondayendlessonsminutes,mondaynumberlessons));
+                        }
+                        else if (dayofweek.toString() == "SATURDAY" || dayofweek.toString() == "SUNDAY")
+                        {
+                            timetxt.setText("ВЫХОДНОЙ");
                         }
                         else
                         {
-                            timetxt.setText(lessons.getNoMonday(hour,minutes,seconds));
+                            timetxt.setText(lessons.getLefttime(hour,minutes,seconds,nomondaystartlessonshours,nomondayendlessonshours,nomondaystartlessonsminutes,nomondayendlessonsminutes,nomondaynumberlessons));
                         }
 
 
@@ -170,20 +155,3 @@ public class MainActivity extends AppCompatActivity
 
     }
 }
-
-
-//                                if(i%2==1)
-//                                {
-//                                    if (conv.getConvert(hour,minutes) <= conv.getConvert(startlessonshours[i] ,startlessonsminutes[i]) & conv.getConvert(hour,minutes) >= conv.getConvert(endlessonshours[i-1] ,endlessonsminutes[i-1]))
-//                                    {
-//                                        timetxt.setText("До конца перемены: " + (conv.getConvert(startlessonshours[i] ,startlessonsminutes[i])-conv.getConvert(hour,minutes)) + " минут.");
-//                                    }
-//                                }
-//                                else if (i%2!=1 & i != 0)
-//                                {
-//                                    if (conv.getConvert(hour,minutes) <= conv.getConvert(startlessonshours[i] ,startlessonsminutes[i]) & conv.getConvert(hour,minutes) >= conv.getConvert(endlessonshours[i-1] ,endlessonsminutes[i-1]))
-//                                    {
-//                                        timetxt.setText("До конца перемены: " + (conv.getConvert(startlessonshours[i] ,startlessonsminutes[i])-conv.getConvert(hour,minutes)) + " минут.");
-//                                    }
-//
-//                                }
