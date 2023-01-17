e21b2f5e8d51f0139f435dda4bcaa1b46fcb3c6e
Owlsovl <105550338+Owlsovl@users.noreply.github.com>
2023-01-18 00:42:21 +0300

    1.2.7

--- app/src/main/java/com/example/timeproject/MainActivity.java
+++ app/src/main/java/com/example/timeproject/MainActivity.java
@@ -1,25 +1,19 @@
 package com.example.timeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
-
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 
-import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 
 import java.time.DayOfWeek;
 import java.time.ZonedDateTime;
-
 import java.util.Timer;
 import java.util.TimerTask;
-
 
 
 class Convert
@@ -69,34 +63,24 @@ class LessonsCalc
         {
             if (terms.getTerms(conv.getConvert(startlessonhours[i], startlessonminutes[i]), conv.getConvert(endlessonhour[i], endlessonminutes[i]), conv.getConvert(hour, minutes)))
             {
-                lefttime = "До конца " + numberlesson[i] + " урока: " + (conv.getConvert(endlessonhour[i], endlessonminutes[i]) - 1 - conv.getConvert(hour, minutes)) + " минут " + (60 - seconds) + " секунд.";
+                lefttime = "До конца " + numberlesson[i] + " урока: " + (conv.getConvert(endlessonhour[i], endlessonminutes[i]) - conv.getConvert(hour, minutes)) + " минут " + (60 - seconds) + " секунд.";
             }
             else if (i != 0)
             {
                 if (terms.getTerms1(conv.getConvert(hour,minutes), conv.getConvert(startlessonhours[i], startlessonminutes[i]), conv.getConvert(endlessonhour[i-1], endlessonminutes[i-1])))
                 {
-                    lefttime = "До конца перемены между " + numberlesson[i-1] + " и " + numberlesson[i]  + " уроками: " + (conv.getConvert(startlessonhours[i] ,startlessonminutes[i])-conv.getConvert(hour,minutes) - 1) + " минут " + (60 - seconds) + " секунд.";
+                    lefttime = "До конца перемены между " + numberlesson[i-1] + " и " + numberlesson[i]  + " уроками: " + (conv.getConvert(startlessonhours[i] ,startlessonminutes[i])-conv.getConvert(hour,minutes)) + " минут " + (60 - seconds) + " секунд.";
                 }
             }
             else //if (conv.getConvert(hour,minutes) > conv.getConvert(endlessonhour[endlessonhour.length-1],endlessonminutes[endlessonminutes.length-1]) & conv.getConvert(hour,minutes) < conv.getConvert(startlessonhours[0],startlessonminutes[0]))
             {
                 // ВРЕМЯ ДО НАЧАЛА ПАР
-                // ДОБАВИТЬ ПРОВЕРКУ ЗАВТРАШНЕГО ДНЯ ЧЕРЕЗ КЛАСС
-                if (conv.getConvert(hour,minutes) > conv.getConvert(startlessonhours[0],startlessonminutes[0]))
-                {
-                    lefttime = (conv.getConvert(24,0) - conv.getConvert(hour,minutes) + conv.getConvert(startlessonhours[0],startlessonminutes[0])) / 60 + ":" + (conv.getConvert(24,0) - conv.getConvert(hour,minutes) + conv.getConvert(startlessonhours[0],startlessonminutes[0])-1) % 60 + ":" + (60 - seconds);
-                    System.out.println(lefttime);
-                    System.out.println("1");
-                }
-                else
-                {
-                    lefttime = (conv.getConvert(startlessonhours[0],startlessonminutes[0]) - conv.getConvert(hour,minutes)) / 60 + ":" + (conv.getConvert(startlessonhours[0],startlessonminutes[0]) - conv.getConvert(hour,minutes)-1) % 60 + ":" + (60 - seconds);
-                    System.out.println(lefttime);
-                    System.out.println("2");
-                }
+//                if (conv.getConvert(hour,minutes) > conv.getConvert(startlessonhours[0], startlessonminutes[0]))
+//                {
 //
-//                lefttime = " 123 ";
-
+//                    lefttime = ((conv.getConvert(24,0) - conv.getConvert(hour, minutes) + conv.getConvert(startlessonhours[0], startlessonminutes[0]))/60) + ":" + ((conv.getConvert(24,0) - conv.getConvert(hour, minutes) + conv.getConvert(startlessonhours[0], startlessonminutes[0]))%60);
+//                }
+                lefttime = String.valueOf(seconds);
             }
         }
         return lefttime;
@@ -125,18 +109,18 @@ public class MainActivity extends AppCompatActivity
     final int[] mondaynumberlessons = new int[] {0,1,2,3,4,5,6,0,7,8,9,10,11,12,13,14};
 
     int lesson;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView timetxt = (TextView) findViewById(R.id.textView);
-        final TextView outtime = (TextView) findViewById(R.id.textView2);
+        TextView timetxt = findViewById(R.id.textView);
+        TextView outtime = findViewById(R.id.textView2);
 
         Timer timer = new Timer();
         LessonsCalc lessons = new LessonsCalc();
-
 
         timer.scheduleAtFixedRate(new TimerTask()
         {
@@ -149,24 +133,30 @@ public class MainActivity extends AppCompatActivity
                     public void run()
                     {
                         ZonedDateTime zone = ZonedDateTime.now();
+
                         int hour = zone.getHour();
                         int minutes = zone.getMinute();
                         int seconds = zone.getSecond();
-                        //DayOfWeek dayofweek = zone.getDayOfWeek();
-                        String dayofweek = "TUESDAY";
-                        System.out.println(dayofweek.toString());
+                        DayOfWeek dayofweek = zone.getDayOfWeek();
+                        //String dayofweek = "MONDAY";
+                        String lt;
+                        //System.out.println(dayofweek.toString());
                         outtime.setText(hour + ":" + minutes + ":" + seconds);
                         if (dayofweek.toString() == "MONDAY")
                         {
-                            timetxt.setText(lessons.getLefttime(hour,minutes,seconds,mondaystartlessonshours,mondayendlessonshours,mondaystartlessonsminutes,mondayendlessonsminutes,mondaynumberlessons));
+                            lt = lessons.getLefttime(hour,minutes,seconds,mondaystartlessonshours,mondayendlessonshours,mondaystartlessonsminutes,mondayendlessonsminutes,mondaynumberlessons);
+                            timetxt.setText(lt);
+                            LaunchService();
                         }
-                        else if (dayofweek.toString() == "SATURDAY" || dayofweek.toString() == "SUNDAY")
+                        else if (dayofweek.toString() == "SATURDAY" || dayofweek.toString() == "SUNDAY" || dayofweek.toString() == "FRIDAY" && hour >= 19)
                         {
                             timetxt.setText("ВЫХОДНОЙ");
                         }
                         else
                         {
-                            timetxt.setText(lessons.getLefttime(hour,minutes,seconds,nomondaystartlessonshours,nomondayendlessonshours,nomondaystartlessonsminutes,nomondayendlessonsminutes,nomondaynumberlessons));
+                            lt = lessons.getLefttime(hour,minutes,seconds,nomondaystartlessonshours,nomondayendlessonshours,nomondaystartlessonsminutes,nomondayendlessonsminutes,nomondaynumberlessons);
+                            timetxt.setText(lt);
+                            LaunchService();
                         }
 
 
@@ -178,51 +168,26 @@ public class MainActivity extends AppCompatActivity
         },0,1000);
 
     }
+
+    private void LaunchService()
+    {
+        Intent intent = new Intent(this, LessonService.class);
+
+        Switch switchbutton = findViewById(R.id.switch1);
+
+        switchbutton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean state) {
+                if(state)
+                {
+                    startService(intent);
+                }
+                else
+                {
+                    stopService(intent);
+                }
+            }
+        });
+    }
 }
 
-//    // Идентификатор уведомления
-//    private int NOTIFY_ID = 101;
-//
-//    // Идентификатор канала
-//    private static final String CHANNEL_ID = "Polytechrasp";
-//
-//    public String str = "Privet;";
-//
-//    int num = 0;
-//
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//
-//    }
-//
-//    public void onClick(View view)
-//    {
-//        // while (true)
-//        // {
-//        //все это 161 строка +-
-//        NotificationCompat.Builder builder
-//                = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID);
-//        builder.setSmallIcon(R.mipmap.ic_launcher);
-//        builder.setContentTitle("Напоминание"); // сюда заголовок по типу  расписание звонков
-//        builder.setContentText("До конца " + "перемены" + " " + num); //здесь lefttime поидеи
-//        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
-//        builder.setOngoing(true);
-//
-//        NotificationManagerCompat notificationManager =
-//                NotificationManagerCompat.from(MainActivity.this);
-//        notificationManager.notify(NOTIFY_ID++, builder.build());
-//        // }
-//        createChannelIfNeeded(notificationManager);
-//
-//    }
-//    public static void createChannelIfNeeded(NotificationManagerCompat manager) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT);
-//            manager.createNotificationChannel(notificationChannel);
-//        }
-//    }
